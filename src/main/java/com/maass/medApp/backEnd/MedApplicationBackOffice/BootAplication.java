c5523c2fd89e3bd3d26a5627ee15eb75817ece65
meetantariksh <meetantariksh@gmail.com>
2017-10-01 13:29:46 +0530

    First Commit

---
+++ src/main/java/com/maass/medApp/backEnd/MedApplicationBackOffice/BootAplication.java
@@ -0,0 +1,25 @@
+package com.maass.medApp.backEnd.MedApplicationBackOffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@ComponentScan({"com.maass.medApp.backEnd"})
+@SpringBootApplication
+@RestController
+@EnableAutoConfiguration
+public class BootAplication {
+	
+	@RequestMapping("/")
+	public String hello() {
+		return "*********************************Medical Back End For Back Office Application is UP and RUNNING*********************************";
+	}
+	
+	public static void main (String args[]){
+		SpringApplication.run(BootAplication.class, args);
+	}
+	
+}
